dca696ce876d558540fe37ec7dd9185e24af5a7b
VioletEvergarden7 <3304135464@qq.com>
2025-03-03 18:14:07 +0800

    Update wfWebsite.py

--- wfWebsite.py
+++ wfWebsite.py
@@ -4,7 +4,7 @@ page = st.sidebar.radio("鸟戾天时：",["首页","信息","下载","我的","
 
 
 def page1():
-    st.balloons()
+    st.snow()
     #文字
     st.image("there_is_whenFly.jpg")
     st.image("welcome.jpg")
@@ -16,7 +16,10 @@ def page2():
     st.image("aboutUs.jpg")
     tab1,tab2,tab3 = st.tabs(["程序逻辑","联系我们","制作者"])
     with tab1:
-        st.write("鸟戾天时的程序毫无逻辑，请不要尝试寻找代码的逻辑。")
+        st.write("你知道吗？这个网站的程序逻辑简单的要命，以下是代码：")
+        for i in range(100):
+            st.write("  ")
+        st.write("不是哥们你真当有啊？")
     with tab2:
         st.write("你把我开了你才可以联系我哦")
     with tab3:
@@ -51,7 +54,26 @@ def page3():
         )
 
 def page4():
-    st.write("你在这里一无所有")
+    tab1,tab2 = st.tabs(["登录","注册"])
+    with tab1:
+        name = st.text_input("请输入您的账号名称")
+        passward = st.text_input("请输入密码")
+        for i in range(100):
+            try:
+                with open("users_data/{0}/{1}/pas.txt".format("c9",i),"r",encoding="utf-8") as f:
+                    list = f.read().split("\n")
+                    if name == list[0]:
+                        if passward == list[1]:
+                            st.write("登录成功！你的账号是：",list[0])
+                            st.write("但是仅仅是登陆成功而已啦，现在账户又没有实际功能呢")
+                        else:
+                            st.write("当前密码不正确，输入完成后请按回车")
+                    else:
+                        st.write("当前账号不正确，输入完成后请按回车")
+            except:
+                pass
+    with tab2:
+        st.write("暂时无法注册")
 
 def page5():
     st.write("本来想在这里放一个付款码的，但是太缺德了，没放:(")
